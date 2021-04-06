3e373cc1ac215126ff8c4107abddf18499da8a25
Benjamin Kim <benjaminsangminkim@gmail.com>
2021-04-06 17:31:27 -0400

    Comments

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -17,7 +17,6 @@ class CustomerSystem{
         exitCondition = "9";
 
         // More variables for the main may be declared in the space below
-        String creditCard = " ";
 
         do{
             printMenu();                                    // Printing out the main menu
@@ -30,7 +29,7 @@ class CustomerSystem{
             }
             else if (userInput.equals(generateCustomerOption)) {
                 // Only the line below may be editted based on the parameter list and how you design the method return
-                generateCustomerDataFile("Valid List", true);
+                generateCustomerDataFile("CustomerList.csv", true);
             }
             else{
                 System.out.println("Please type in a valid option (A number from 1-9)");
@@ -84,62 +83,74 @@ class CustomerSystem{
     }
     /*
     * Author - Benjamin Kim
-    * Description - Takes a credit card number and outputs if it is valid or not
+    * Description - Takes a credit card number with more than 9 characters and outputs if it is valid or not
     * 
     * @param - String creditCard
     * @return - booleanValid
     */
     public static String validateCreditCard() {
         Scanner user = new Scanner(System.in);
-        System.out.println("Enter credit card number");
-        String creditCard = user.nextLine();
+    
         //Variables
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        int sum;
-        int oddSum = 0;
-        int evenSum = 0;
-        int sumDoubleDigit = 0;
-        int sumSingleDigit = 0;
-        boolean valid = false;
+        String creditCard = " ";    //Blank string so the string can be used outside while loop
+        int count = 0;  //Count for while loop
+        int count1 = 0; //Count for for loop that reverses string. Is one less than count because of how a computer counts digits
+        int count2 = 0; //Count for reversed credot card. Need to count it again to use in other for loop
+        int count3 = 0; //Count for even digits that equal more than 10 when doubled
+        int sum;    //Overall sum of credit card after all processes are done
+        int oddSum = 0; //Sum of the odd digits
+        int evenSum = 0;    //Overall sum of even digits
+        int sumDoubleDigit = 0; //Sum of even digits that had 2 digits after doubling
+        int sumSingleDigit = 0; //Sum of even digits that had only 1 digit after doubling
+
+        while(count < 9) {  //While loop for user to input credit card number
+            System.out.println("Enter credit card number");
+            creditCard = user.nextLine();   //String instead of int since int can't hold that high of a value
+            count = creditCard.length();    //Count the length
+            if (count >= 9) {   //If the string is more than 9 characters, its valid
+                System.out.println("Number is long enough");
+            }
+            else {  //Anything less than 9 and it goes back to the top of the loop
+                System.out.println("Number is not long enough");
+            }
+        }
 
         //Counting characters
-        count1 = creditCard.length() - 1;
-        StringBuilder sb = new StringBuilder();
+        count1 = count - 1; //if count above is 9, the digits are going to be 0 - 8, therefore you need minus 1
+        StringBuilder sb = new StringBuilder(); //String builder: Something I found on google so that I can get a variable to work with
 
         //Reversing the numbers
-        for (int i = 0; i <= count1; i++) {
-            char character = creditCard.charAt(count1 - i);
-            sb.append(character);
+        for (int i = 0; i <= count1; i++) { //For loop to reverse number
+            char character = creditCard.charAt(count1 - i); //Digits switch positions
+            sb.append(character);   //String builder adds the digits each time together to form a string
         }
-        String reverseCreditCard = sb.toString();
+        String reverseCreditCard = sb.toString();   //Storing the reversed number
 
         //count for reversed credit card number so the digits are now correctly in place
         count2 = reverseCreditCard.length() - 1;
 
         //Finding the odd and even values
-        for (int i = 0; i <= count2; i++) {
-            char digit = reverseCreditCard.charAt(i);
-            int digitNum = digit - '0';
+        for (int i = 0; i <= count2; i++) { //Separating the characters again
+            char digit = reverseCreditCard.charAt(i);   //Finding each character in each digit
+            int digitNum = digit - '0'; //Switching the characters to separate integers to work with
 
             //While loop to add all the odd digits
-            while(i%2 == 0) {
-                oddSum = oddSum + digitNum;
+            while(i%2 == 0) {   //Since it counts 0 - int, the digits would shift, making even digits to the computer, odd digits to us
+                oddSum = oddSum + digitNum; //Add the number to a variable each time
                 break;
             }
 
             //While loop to calculate the even digits
-            while(i%2 != 0) {
-                int even = digitNum*2;
+            while(i%2 != 0) {   //Since it counts 0 - int, the digits would dhift, making odd digits to the computer, even digits to us
+                int even = digitNum*2;  //Multipling numbers by 2
 
                 //If even digit is more than 9, this happens
-                if (even > 9) {   
+                if (even > 9) { //After doubling, the number is greater than 9, it goes through this loop
                     count3 = String.valueOf(even).length() - 1;   //Get the length of the string
                     for (int j = 0; j <= count3; j++) {    //Getting the two characters to add them together
-                        char evenDigit = String.valueOf(even).charAt(j);
+                        char evenDigit = String.valueOf(even).charAt(j);    //Getting value of each digit
                         int evenDigitNum = evenDigit - '0';    //Changing the character to a integer value so it can be added together
-                        sumDoubleDigit = sumDoubleDigit + evenDigitNum; //Adding the digits to 0 to get sum of even numbers that are greater than 9 after doubling
+                        sumDoubleDigit = sumDoubleDigit + evenDigitNum; //Adding the digits together
                     }
                 }
                 else if (even <= 9) {    //When it does not become double digits
@@ -148,18 +159,14 @@ class CustomerSystem{
                 break;
             }
         }
-        evenSum = sumDoubleDigit + sumSingleDigit;
-        
-        //Final Sum
-        sum = oddSum + evenSum;
+        evenSum = sumDoubleDigit + sumSingleDigit;  //Adding 2 different types of even digits together
+        sum = oddSum + evenSum; //Final sum of odd and even digits
 
         //Finding if the card is valid
         if (sum%10 == 0) {  //If its ending in '0' it has to be divisible by 10 unless it is 0, but thats not possible.
-            valid = true;
             return "Card is valid";
         }
         else {  //Any number not divisible by 10 doesn not end in 0.
-            valid = false;
             return "Card is not valid";
         }
     }
@@ -172,17 +179,25 @@ class CustomerSystem{
     * @return - No return. It is a void method
     */
     public static void generateCustomerDataFile(String file, boolean addInfo){
-        String customerInfo = enterCustomerInfo();
+        //Just in case
+        Random rand = new Random(); //Random in case unique id doesn't work
+        int id = rand.nextInt(1000000); //Making range very big
+
+        //Calling other methods to add to CSV file
+        String customerInfo = enterCustomerInfo();  
         String validCard = validateCreditCard();
-        int id = newId("Valid List");
+        //int id = newId("Valid List"); This method may not work but added in case Mr. Ho wants to look at it
+
         try {   //Runs this code
-            //Creating the file
-            File fileName = new File(file);
-            FileWriter fileWriter = new FileWriter(fileName, addInfo);
+            //Creating a file. File name is the same everytime. It is fixed in the main
+            File fileName = new File(file); 
+            //Creates fileWriter. Needs file name and true to continue. True is fixed in the main
+            FileWriter fileWriter = new FileWriter(fileName, addInfo); 
+            //PrintWriter which allows program to print lines
             PrintWriter printWriter = new PrintWriter(fileWriter);
 
             //Text in the file
-            printWriter.println(id + ", " + customerInfo + ", " + validCard);
+            printWriter.println(id + ", " + customerInfo + ", " + validCard);   //Using the methods
     
             //Closing so that the lines actually print
             printWriter.close();
@@ -194,29 +209,44 @@ class CustomerSystem{
     /*******************************************************************
     *       ADDITIONAL METHODS MAY BE ADDED BELOW IF NECESSARY         *
     *******************************************************************/
+    /* 
+     * The methods below are what is used to make the unique ID number. 
+     * It didn't work so I put it in comments below in case Mr. Ho wants to see it
+     */
+    
+    /*
+     * Author - Benjamin Kim
+     * Description - takes the last line from the CSV file, reads it , and stores it as a string
+     * 
+     * @param - File name
+     * @return - Last Line in file
+     */
+    /*
     public static String lastLine (String file) {
         String line;
         String lastLine = " ";
-        Scanner scan = new Scanner(file);
-        if (scan.hasNextLine()) {
-            try {
-                BufferedReader input = new BufferedReader(new FileReader(file));
-                while ((line = input.readLine()) != null) {
-                    if (line == " ") {
-                        break;
-                    }
-                    lastLine = line;
+        try {
+            BufferedReader input = new BufferedReader(new FileReader(file));
+            while ((line = input.readLine()) != null) {
+                if (line == " ") {
+                    break;
                 }
-            } catch (IOException e) {
-                System.out.println("Error has occured");
+                lastLine = line;
             }
-        }
-        else {
-            return " ";
+        } catch (IOException e) {
+            System.out.println("Error has occured");
         }
         return lastLine;
-    }
+    } */
 
+    /*
+     * Author - Benjamin Kim
+     * Description - Takes length of last line and takes out the length of the ID number
+     * 
+     * @param - Last line
+     * @return - Amount of digits according to a computer
+     */
+    /*
     public static int numLength(String line) {
         int lineLength = line.length(); //Get the length of the last line in the text file
         for (int i = 0; i <= lineLength; i++) {
@@ -225,18 +255,35 @@ class CustomerSystem{
             }
         }
         return 0;
-    }
+    } */
+
+    /*
+     * Author - Benjamin Kim
+     * Description - Gets the actual integer value of ID number
+     * 
+     * @param - Last line and where the number ends
+     * @return - Old Id number
+     */
+    /*
     public static int getIdNumber(String line, int comma) {
         String idNumStr = line.substring(0, comma);
         int idNumber = Integer.parseInt(idNumStr); 
         return idNumber;
-    }
+    } */
 
+    /*
+     * Author - Benjamin Kim
+     * Description - Creates new ID number
+     * 
+     * @param - File name
+     * @return - New ID number
+     */
+    /*
     public static int newId(String file) {
         String lastLine = lastLine(file);
         int length = numLength(lastLine);
         int oldIdNum = getIdNumber(lastLine, length);
         int id = oldIdNum + 1;
         return id;
-    }
+    } */
 }
